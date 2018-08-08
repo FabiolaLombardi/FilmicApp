a0a0b0e97604ea768960873a9d1628643131ded5
Carlos <carlosdima_97@hotmail.com>
2018-08-08 02:41:25 -0400

    added catalog page and add to cart function (need full back to continue)

---
+++ frontEnd/js/product/home.js
@@ -0,0 +1,18 @@
+get('category/get').then((res) => {
+    res.body.map((category,index) =>{
+        let container = document.getElementById('categorias')
+        if(index%3===0){
+          container.innerHTML+=`<div class="w-100"></div>`
+        }
+        container.innerHTML+=`
+                      <div class="col-sm">
+                          <div class="card" >
+                              <img class="card-img-top" src="img/Comedia2.jpg" alt="Card image cap">
+                              <div class="card-body">
+                                <a href="catalog.html?cat=${category.name}" class="btn">Ir a ${category.name}</a>
+                              </div>
+                            </div>
+                      </div>
+                      `
+    })
+})
